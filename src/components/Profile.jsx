5f81473070d4d04d2ddc1bac19f3242880dfda15
AntL1n <antlin96@gmail.com>
2021-08-03 15:34:09 +0300

    react add

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () =>{
+    return   <div className="content">
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
+    </div>
+    <div>ava + description</div>
+    <div>
+      My Post<div>New post</div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
